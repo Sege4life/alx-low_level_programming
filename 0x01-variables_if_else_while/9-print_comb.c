5e68aa2ed0e6bf60d45c18d958c05c70ab8c4b17
Sege4life <folayemi.oluwasegun@gmail.com>
2022-09-10 15:59:17 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,44 +1,24 @@
-/*
-*
-* File: 9-print_comb.c
-*
-* Auth: Brennan D Baraban 
-*/
-
-
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
-*
-* main - Prints all possible combinations of single-digit numbers.
-*
-*
-* Return: Always 0.
-*
-*/
-
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
 int main(void)
-
 {
-int num;
-
-for (num = 0; num <= 9; num++)
-
+int c = 0;
+while (c < 10)
 {
-putchar((num % 10) + '0');
-
-if (num == 9)
-
-continue;
-											
-
+putchar(48 + c);
+if (c != 9)
+{
 putchar(',');
-
 putchar(' ');
-
 }
-
+c++;
+}
 putchar('\n');
 return (0);
-
 }
