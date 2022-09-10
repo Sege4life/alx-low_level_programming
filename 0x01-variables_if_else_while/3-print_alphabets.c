dd87bbc53cf23e19d6e18d712a9be5fcc308a3d9
Sege4life <folayemi.oluwasegun@gmail.com>
2022-09-09 21:32:48 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,14 @@
  */
 int main(void)
 {
-	int ch;
-	for (ch = 'a'; ch <= 'z'; ch++)
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 putchar(ch);
 }
 for (ch = 'A'; ch <= 'Z'; ch++)
 {
-	putchar(ch);
+putchar(ch);
 }
 putchar('\n');
 return (0);
