dbffb40b63a4ce7ef937b04dd78288be44132cac
Sege4life <folayemi.oluwasegun@gmail.com>
2022-09-11 15:55:02 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- *
  * main - Prints a hexadecimal string
+ *
  *
  *
  * Return: Always (Success)
@@ -13,34 +12,22 @@ int main(void)
 
 {
 
-		char c;
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
+
+for (c = 'a'; c <= 'f'; c++)
+{
+
+putchar(c);
+
+}
 
 
+putchar('\n');
 
-			for (c = '0'; c <= '9'; c++)
-
-					{
-
-								putchar(c);
-
-									}
-
-
-
-				for (c = 'a'; c <= 'f'; c++)
-
-						{
-
-									putchar(c);
-
-										}
-
-
-
-					putchar('\n');
-
-
-
-						return (0);
+return (0);
 
 }
