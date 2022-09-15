4308755a0418659589c0e1a5c3448146aeeaab0a
Sege4life <folayemi.oluwasegun@gmail.com>
2022-09-15 15:30:39 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,11 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
-void positive_or_negative(int i);
+void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
-void print_times_table(int n);
+void print_remaining_days(int month, int day, int year);
 
-#endif
+#endif /* MAIN_H */
