2f41654320285e2d3bc12c412020f985b2e8f313
Sege4life <folayemi.oluwasegun@gmail.com>
2022-12-17 14:40:40 -0800

    Dynamiclib Review

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com./Giftvictor12/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
+wget -P /tmp/ https://raw.github.com./Sege4life/alx-low_level_programming/main/0x18-dynamic_libraries/libruin.so
 export LD_PRELOAD=/tmp/libruin.so
